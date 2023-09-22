d2333afdf6f9123c41925ca76a6e95cc73b872b2
Roger Silva <rogersilvati@gmail.com>
2023-09-22 20:23:36 -0300

    Project started

---
+++ projeto-matera-bootcamp-bank/src/main/java/com/matera/bootcampbank/materabootcampbank/MateraBootcampBankApplication.java
@@ -0,0 +1,13 @@
+package com.matera.bootcampbank.materabootcampbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MateraBootcampBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MateraBootcampBankApplication.class, args);
+	}
+
+}
---
+++ projeto-matera-bootcamp-bank/src/test/java/com/matera/bootcampbank/materabootcampbank/MateraBootcampBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matera.bootcampbank.materabootcampbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MateraBootcampBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
