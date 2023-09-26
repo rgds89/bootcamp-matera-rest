d5a30e85da9b1e1677fd5960223504ce03562f42
Roger Silva <rogersilvati@gmail.com>
2023-09-26 20:25:35 -0300

    Criado projeto bacen

---
+++ projeto-matera-bootcamp-bacen/src/test/java/com/matera/bootcampbacen/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matera.bootcampbacen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
